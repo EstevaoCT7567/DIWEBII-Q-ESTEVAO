05e1af8752b07a34ebd173cfa1ef4ef01b21554b
danielfd-amorim <daniel.dan.farias2008@gmail.com>
2025-10-02 21:42:07 -0300

    aula de hoje

--- Aula 11 - Javascript/alerta.js
+++ Aula 11 - Javascript/alerta.js
@@ -1,1 +1,62 @@
-alert('Olá mundo!!');
+// alert('Olá mundo!!');
+//operadores de comparação
+// == igual a
+// != diferente de
+// < menor que
+// > maior que
+// <= menor ou igual a
+// >= maior ou igual a
+
+var valor1 = 7;
+var valor2 = 7;
+
+if (valor1 == valor2) {
+    document.write(valor1 ,' é igual a  ',valor2,"<br><br>");
+} else {
+    document.write(valor1,' é diferente de ',valor2,"<br><br>");
+}
+
+var valor1 = 8;
+var valor2 = 9;
+
+if (valor1 != valor2) {
+    document.write(valor1 ,' é diferente de  ',valor2,"<br><br>");
+} else {
+    document.write(valor1,' é igual a ',valor2,"<br><br>");
+}
+
+var valor1 = 8;
+var valor2 = 7;
+
+if (valor1 < valor2) {
+    document.write(valor1 ,' é menor que  ',valor2,"<br><br>");
+} else {
+    document.write(valor1,' é maior que ',valor2,"<br><br>");
+}
+
+var valor1 = 8;
+var valor2 = 7;
+
+if (valor1 > valor2) {
+    document.write(valor1 ,' é maior que  ',valor2,"<br><br>");
+} else {
+    document.write(valor1,' é menor que ',valor2,"<br><br>");
+}
+
+var valor1 = 8;
+var valor2 = 7;
+
+if (valor1 <= valor2) {
+    document.write(valor1 ,' é menor ou igual a  ',valor2,"<br><br>");
+} else {
+    document.write(valor1,' é maior ou igual ',valor2,"<br><br>");
+}
+
+var valor1 = 8;
+var valor2 = 7;
+
+if (valor1 >= valor2) {
+    document.write(valor1 ,' é maior ou igual a ',valor2,"<br><br>");
+} else {
+    document.write(valor1,' é menor ou igual a ',valor2,"<br><br>");
+}
