def0013119b450d4b2cfa5757b60020caa75df57
Andrey <>
2025-10-09 22:02:14 -0300

    aulade hj

--- Aula 13 - Javascript - Condicionais/index.js
+++ Aula 13 - Javascript - Condicionais/index.js
@@ -10,15 +10,44 @@
 //     document.write(nota + ' - Reprovado');
 // }
 
-var sol = false;
-var chuva = false;
+// var sol = false;
+// var chuva = false;
 
-if (sol) {
-    document.write('Vamos a praia, pois está fazendo sol');
-} else if(chuva){
-    document.write('Não vamos a praia, pois está chovendo');
-} else {
-    document.write('Vamos ao parte, pois está nulado');
-}
+// if (sol) {
+//     document.write('Vamos a praia, pois está fazendo sol');
+// } else if(chuva){
+//     document.write('Não vamos a praia, pois está chovendo');
+// } else {
+//     document.write('Vamos ao parte, pois está nulado');
+// }
 
 //Estrutura condicional Switch
+
+// var key = 5;
+
+// switch (key) {
+//     case 1:
+//         document.write('Chave: ' + key + ' - A chave é do apartamento');
+//         break;
+//     case 2:
+//         document.write('Chave: ' + key + ' - A chave é da casa');
+//         break;
+//     case 3:
+//         document.write('Chave: ' + key + ' - A chave é do carro');
+//         break;
+//     case 4:
+//         document.write('Chave: ' + key + ' - A chave é da moto');
+//         break;
+//     default:
+//         document.write('Chave: ' + key + ' - Não coresponde a nenhum uso');
+//         break;
+// }
+
+// while
+
+var inicio = 0;
+
+while (inicio <= 10000) {
+    document.write('Numero do cliente: ' + inicio + '<br>');
+    inicio++;
+}
