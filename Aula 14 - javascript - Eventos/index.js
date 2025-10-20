0b34685e2bff44cb202e27a70b1f246b8dfbb039
Andrey <>
2025-10-20 20:45:08 -0300

    aula de hoje

---
+++ Aula 14 - javascript - Eventos/index.js
@@ -0,0 +1,7 @@
+function carConteudo() {
+    document.getElementById("conteudo").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nobis animi vero inventore, quas eum quod obcaecati vitae sequi facere ea dolor maxime laudantium culpa veniam ad fuga voluptates magnam?";
+}
+
+function mouseOver() {
+    document.getElementById("mouseAlt").innerHTML = "Retire o Mouse";
+}
